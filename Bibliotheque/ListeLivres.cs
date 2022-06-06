820fdecdd40a2d2a71c09cd1166a82d4e8c4a77c
Yann Claudon <yann.claudon@gmail.com>
2022-06-06 19:36:41 +0200

    Modification form Liste Livre -Ajout de la fonctionnalité recherche de livre -Affichage des livres dans datagridview directement par SQL -Suppression d'un livre lors d'un clic sur le bouton fonctionnel

--- Bibliotheque/ListeLivres.cs
+++ Bibliotheque/ListeLivres.cs
@@ -63,17 +63,25 @@ namespace Bibliotheque{
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btn_modifier_Click(object sender, EventArgs e){ 
-            SqlConnection con = new SqlConnection("Data Source =.\\sqlexpress; Initial Catalog = bibliotheque; Integrated Security = True");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("UPDATE book SET name_book = @Nom, publish_date_book = @Publication,  WHERE id_livre = @ID", con);
-            cmd.Parameters.AddWithValue("@Nom", this.dgridview_livre.CurrentRow.Cells[1].Value.ToString());
-            cmd.Parameters.AddWithValue("@Naissance", this.dgridview_livre.CurrentRow.Cells[2].Value.ToString());
-            cmd.Parameters.AddWithValue("@ID", this.dgridview_livre.CurrentRow.Cells[0].Value.ToString());
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Modification du livre effectué");            
+            
+            if (this.dgridview_livre.SelectedRows.Count > 0){
+                SqlConnection con = new SqlConnection("Data Source =.\\sqlexpress; Initial Catalog = bibliotheque; Integrated Security = True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE book SET name_book = @Nom, publish_date_book = @Publication, fk_genre_book = @Genre, fk_author_book = @Auteur, fk_format_book = @Format, fk_editor_book = @Editeur, fk_status_book = @Etat WHERE id_book = @ID", con);
+                cmd.Parameters.AddWithValue("@Nom", this.dgridview_livre.CurrentRow.Cells[1].Value.ToString());
+                cmd.Parameters.AddWithValue("@ID", this.dgridview_livre.CurrentRow.Cells[0].Value.ToString());
+                cmd.Parameters.AddWithValue("@Genre", this.dgridview_livre.CurrentRow.Cells[3].Value.ToString());
+                cmd.Parameters.AddWithValue("@Auteur", this.dgridview_livre.CurrentRow.Cells[5].Value.ToString());
+                cmd.Parameters.AddWithValue("@Format", this.dgridview_livre.CurrentRow.Cells[6].Value.ToString());
+                cmd.Parameters.AddWithValue("@Editeur", this.dgridview_livre.CurrentRow.Cells[4].Value.ToString());
+                cmd.Parameters.AddWithValue("@Publication", this.dgridview_livre.CurrentRow.Cells[2].Value.ToString());
+                cmd.Parameters.AddWithValue("@Etat", this.dgridview_livre.CurrentRow.Cells[7].Value.ToString());
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Modification du livre effectué");
+            }
         }
-
+        
         /// <summary>
         /// Recherche d'un livre par son nom en fonction de ce qui est entré dans la textbox
         /// </summary>
@@ -87,7 +95,8 @@ namespace Bibliotheque{
             var commandBuilder = new SqlCommandBuilder(dataAdapter);
             var ds = new DataSet();
             dataAdapter.Fill(ds);
-            dgridview_livre.DataSource = ds.Tables[0];            
+            dgridview_livre.DataSource = ds.Tables[0];
+
         }
     }
 }
