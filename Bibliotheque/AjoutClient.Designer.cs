3ac44e0c8b27e2c94577aa3b4b9916d5de2a635b
Raphates <raphael.bally@hotmail.fr>
2022-06-07 18:26:10 +0200

    Mise à jour du formulaire et rectification de quelques bugs

--- Bibliotheque/AjoutClient.Designer.cs
+++ Bibliotheque/AjoutClient.Designer.cs
@@ -31,9 +31,9 @@
             this.gBox_AjoutAuteur = new System.Windows.Forms.GroupBox();
             this.txtbox_ville_client = new System.Windows.Forms.TextBox();
             this.txtbox_prenom_client = new System.Windows.Forms.TextBox();
-            this.btn_annulerAuteur = new System.Windows.Forms.Button();
+            this.btn_annulerClient = new System.Windows.Forms.Button();
             this.txtbox_telephone_client = new System.Windows.Forms.TextBox();
-            this.btn_ajouterAuteur = new System.Windows.Forms.Button();
+            this.btn_ajouterClient = new System.Windows.Forms.Button();
             this.txtbox_nom_client = new System.Windows.Forms.TextBox();
             this.gBox_AjoutAuteur.SuspendLayout();
             this.SuspendLayout();
@@ -42,9 +42,9 @@
             // 
             this.gBox_AjoutAuteur.Controls.Add(this.txtbox_ville_client);
             this.gBox_AjoutAuteur.Controls.Add(this.txtbox_prenom_client);
-            this.gBox_AjoutAuteur.Controls.Add(this.btn_annulerAuteur);
+            this.gBox_AjoutAuteur.Controls.Add(this.btn_annulerClient);
             this.gBox_AjoutAuteur.Controls.Add(this.txtbox_telephone_client);
-            this.gBox_AjoutAuteur.Controls.Add(this.btn_ajouterAuteur);
+            this.gBox_AjoutAuteur.Controls.Add(this.btn_ajouterClient);
             this.gBox_AjoutAuteur.Controls.Add(this.txtbox_nom_client);
             this.gBox_AjoutAuteur.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gBox_AjoutAuteur.Location = new System.Drawing.Point(12, 12);
@@ -63,7 +63,6 @@
             this.txtbox_ville_client.Size = new System.Drawing.Size(100, 25);
             this.txtbox_ville_client.TabIndex = 7;
             this.txtbox_ville_client.Text = "Code Postal";
-            this.txtbox_ville_client.TextChanged += new System.EventHandler(this.txtbox_ville_client_TextChanged);
             this.txtbox_ville_client.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbox_ville_client_KeyPress);
             // 
             // txtbox_prenom_client
@@ -75,16 +74,16 @@
             this.txtbox_prenom_client.TabIndex = 6;
             this.txtbox_prenom_client.Text = "Prénom";
             // 
-            // btn_annulerAuteur
+            // btn_annulerClient
             // 
-            this.btn_annulerAuteur.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_annulerAuteur.Location = new System.Drawing.Point(140, 125);
-            this.btn_annulerAuteur.Name = "btn_annulerAuteur";
-            this.btn_annulerAuteur.Size = new System.Drawing.Size(75, 23);
-            this.btn_annulerAuteur.TabIndex = 5;
-            this.btn_annulerAuteur.Text = "Annuler";
-            this.btn_annulerAuteur.UseVisualStyleBackColor = true;
-            this.btn_annulerAuteur.Click += new System.EventHandler(this.btn_annulerAuteur_Click);
+            this.btn_annulerClient.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_annulerClient.Location = new System.Drawing.Point(140, 125);
+            this.btn_annulerClient.Name = "btn_annulerClient";
+            this.btn_annulerClient.Size = new System.Drawing.Size(75, 23);
+            this.btn_annulerClient.TabIndex = 5;
+            this.btn_annulerClient.Text = "Annuler";
+            this.btn_annulerClient.UseVisualStyleBackColor = true;
+            this.btn_annulerClient.Click += new System.EventHandler(this.btn_annulerClient_Click);
             // 
             // txtbox_telephone_client
             // 
@@ -95,19 +94,18 @@
             this.txtbox_telephone_client.Size = new System.Drawing.Size(100, 25);
             this.txtbox_telephone_client.TabIndex = 4;
             this.txtbox_telephone_client.Text = "Téléphone";
-            this.txtbox_telephone_client.TextChanged += new System.EventHandler(this.txtbox_telephone_client_TextChanged);
             this.txtbox_telephone_client.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbox_telephone_client_KeyPress);
             // 
-            // btn_ajouterAuteur
+            // btn_ajouterClient
             // 
-            this.btn_ajouterAuteur.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_ajouterAuteur.Location = new System.Drawing.Point(6, 125);
-            this.btn_ajouterAuteur.Name = "btn_ajouterAuteur";
-            this.btn_ajouterAuteur.Size = new System.Drawing.Size(75, 23);
-            this.btn_ajouterAuteur.TabIndex = 2;
-            this.btn_ajouterAuteur.Text = "Ajouter";
-            this.btn_ajouterAuteur.UseVisualStyleBackColor = true;
-            this.btn_ajouterAuteur.Click += new System.EventHandler(this.btn_ajouterAuteur_Click);
+            this.btn_ajouterClient.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ajouterClient.Location = new System.Drawing.Point(6, 125);
+            this.btn_ajouterClient.Name = "btn_ajouterClient";
+            this.btn_ajouterClient.Size = new System.Drawing.Size(75, 23);
+            this.btn_ajouterClient.TabIndex = 2;
+            this.btn_ajouterClient.Text = "Ajouter";
+            this.btn_ajouterClient.UseVisualStyleBackColor = true;
+            this.btn_ajouterClient.Click += new System.EventHandler(this.btn_ajouterClient_Click);
             // 
             // txtbox_nom_client
             // 
@@ -136,9 +134,9 @@
         #endregion
 
         private System.Windows.Forms.GroupBox gBox_AjoutAuteur;
-        private System.Windows.Forms.Button btn_annulerAuteur;
+        private System.Windows.Forms.Button btn_annulerClient;
         private System.Windows.Forms.TextBox txtbox_telephone_client;
-        private System.Windows.Forms.Button btn_ajouterAuteur;
+        private System.Windows.Forms.Button btn_ajouterClient;
         private System.Windows.Forms.TextBox txtbox_nom_client;
         private System.Windows.Forms.TextBox txtbox_prenom_client;
         private System.Windows.Forms.TextBox txtbox_ville_client;
