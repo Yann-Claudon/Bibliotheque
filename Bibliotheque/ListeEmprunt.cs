1c6594a42171234be183f86dfed80701d70e0d27
Yann-Claudon <yann.claudon@gmail.com>
2022-06-07 17:04:03 +0200

    Modification du form accueil -Supression des boutons planning, prêt et retour de livre
    Ajout du form liste Emprunt
    -Ajout d'un datagridview
    -Ajout d'un bouton impression
    -Ajout de deux bouton prêt et retour de livre
    
    Ajout du form Pret Livre
    Ajout du form Retour Livre
    Suppression du form Planning
    Ajout de commentaires
    Ajout d'un Projet de Test

---
+++ Bibliotheque/ListeEmprunt.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Bibliotheque{
+    public partial class ListeEmprunt : Form{
+        /*
+         * Constructeur
+         */
+        public ListeEmprunt(){
+            InitializeComponent();
+        }
+        /*
+         * Affiche le formulaire de pret d'un livre
+         */
+        private void btn_pretLivre_Click(object sender, EventArgs e){
+            PretLivre pretLivre = new PretLivre();
+            pretLivre.ShowDialog();
+        }
+        /*
+         * Ferme la page actuelle et ouvre la page d'accueil
+         */
+        private void btn_accueil_Click(object sender, EventArgs e){
+            Accueil accueil = new Accueil();
+            accueil.ShowDialog();
+            Close();
+        }
+
+        /*
+         * Charge les données des tables book, genre, author, format, editor, status, loan et client pour pouvoir les afficher dans le DataGridView
+         */
+        private void ListeEmprunt_Load(object sender, EventArgs e){
+            var select = "SELECT id_book AS 'ID livre', name_book AS 'Nom du livre', name_genre AS 'Genre',name_status AS 'Etat', name_client AS 'Nom emprunteur',first_date_loan AS 'Date de prêt', last_date_loan AS 'Date de retour' FROM book INNER JOIN genre ON book.fk_genre_book = genre.id_genre INNER JOIN author ON book.fk_author_book = author.id_author INNER JOIN format ON book.fk_format_book = format.id_format INNER JOIN editor ON book.fk_editor_book = editor.id_editor INNER JOIN status ON book.fk_status_book = status.id_status INNER JOIN loan ON book.id_book = loan.fk_book_loan INNER JOIN client ON loan.fk_client_loan = client.id_client ; ";
+            var c = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=bibliotheque;Integrated Security=True"); 
+            var dataAdapter = new SqlDataAdapter(select, c);
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dgridview_livre.DataSource = ds.Tables[0];
+        }
+        /*
+         * Recherche d'un emprunt en fonction du nom livre ou date de retour entré dans le champ de recherche et l'affiche dans le DataGridView
+         */
+        private void txtbox_recherche_TextChanged(object sender, EventArgs e){
+            var select = "SELECT id_book AS 'ID livre', name_book AS 'Nom du livre', name_genre AS 'Genre',name_status AS 'Etat', name_client AS 'Nom emprunteur',first_date_loan AS 'Date de prêt', last_date_loan AS 'Date de retour' FROM book INNER JOIN genre ON book.fk_genre_book = genre.id_genre INNER JOIN author ON book.fk_author_book = author.id_author INNER JOIN format ON book.fk_format_book = format.id_format INNER JOIN editor ON book.fk_editor_book = editor.id_editor INNER JOIN status ON book.fk_status_book = status.id_status INNER JOIN loan ON book.id_book = loan.fk_book_loan INNER JOIN client ON loan.fk_client_loan = client.id_client WHERE name_book LIKE '%' + @Nom + '%' OR  last_date_loan LIKE '%' + @Dateretour + '%';";
+            var c = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=bibliotheque;Integrated Security=True");
+            var dataAdapter = new SqlDataAdapter(select, c);
+            dataAdapter.SelectCommand.Parameters.AddWithValue("@Nom", txtbox_recherche.Text);
+            dataAdapter.SelectCommand.Parameters.AddWithValue("@Dateretour", txtbox_recherche.Text);
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dgridview_livre.DataSource = ds.Tables[0];
+
+        }
+        /*
+         * Affiche le formulaire de retour d'un livre
+         */
+        private void btn_retourLivre_Click(object sender, EventArgs e)
+        {
+            RetourLivre retourLivre = new RetourLivre();
+            retourLivre.ShowDialog();
+        }
+
+        /*
+         * Ouvre une boite de dialogue pour choisir son imprimante puis une boite de dialogue de prévisualisation de l'impression
+         */
+        Bitmap bmp;
+        private void btn_imprimer_Click(object sender, EventArgs e)
+        {
+            printDialog1.ShowDialog();
+            Graphics g = this.CreateGraphics();
+            bmp = new Bitmap(this.Width, this.Height);
+            Graphics mg = Graphics.FromImage(bmp);
+            mg.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, this.Size);
+            printPreviewDialog1.ShowDialog();
+        }
+
+        /*
+         * Imprime le Form
+         */
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(bmp, 0, 0);
+        }
+    }
+}
--- Bibliotheque/PretLivre.cs
+++ Bibliotheque/PretLivre.cs
@@ -52,37 +52,38 @@ namespace Bibliotheque{
         }
 
         /*
-         * Insére les données dans la table pret quand le bouton valider est cliqué
-         */
-        private void btn_pret_Click(object sender, EventArgs e){       
-                    SqlConnection con = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=bibliotheque;Integrated Security=True");
-                    con.Open();
-                    SqlCommand cmdbook = new SqlCommand("SELECT id_book FROM book WHERE name_book LIKE '" + cmbbox_livre.Text + "'", con);
-                    SqlDataReader drgbook = cmdbook.ExecuteReader();
-                    drgbook.Read();
-                    int id_book = Convert.ToInt32(drgbook["id_book"]);
-                    drgbook.Close();
-
-                    SqlCommand cmdclient = new SqlCommand("SELECT id_client FROM clienb WHERE name_client LIKE '" + cmbbox_nomClient.Text + "'", con);
-                    SqlDataReader drclient = cmdclient.ExecuteReader();
-                    drclient.Read();
-                    int id_client = Convert.ToInt32(drclient["id_client"]);
-                    drclient.Close();
-
-                    SqlCommand cmd = new SqlCommand("INSERT INTO loan(fk_book_loan,first_date_loan, last_date_loan, fk_client_loan) VALUES(@id_book,@date_pret,@date_retour,@id_client)", con);
-                    cmd.Parameters.AddWithValue("@id_book", id_book);
-                    cmd.Parameters.AddWithValue("@date_pret", date_pret.Value);
-                    cmd.Parameters.AddWithValue("@date_retour", date_retour.Value);
-                    cmd.Parameters.AddWithValue("@id_client", id_client);
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-        }
-
-        /*
          * Ferme le formulaire lors du clic sur le bouton annuler
          */
         private void btn_annulerAuteur_Click(object sender, EventArgs e){
+            Close();
+        }
+
+        /*
+        * Insére les données dans la table pret quand le bouton valider est cliqué
+        */
+        private void btn_ajouterAuteur_Click(object sender, EventArgs e){
+            SqlConnection con = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=bibliotheque;Integrated Security=True");
+            con.Open();
+            SqlCommand cmdbook = new SqlCommand("SELECT id_book FROM book WHERE name_book LIKE '" + cmbbox_livre.Text + "'", con);
+            SqlDataReader drgbook = cmdbook.ExecuteReader();
+            drgbook.Read();
+            int id_book = Convert.ToInt32(drgbook["id_book"]);
+            drgbook.Close();
+
+            SqlCommand cmdclient = new SqlCommand("SELECT id_client FROM client WHERE name_client LIKE '" + cmbbox_nomClient.Text + "'", con);
+            SqlDataReader drclient = cmdclient.ExecuteReader();
+            drclient.Read();
+            int id_client = Convert.ToInt32(drclient["id_client"]);
+            drclient.Close();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO loan(fk_book_loan,first_date_loan, last_date_loan, fk_client_loan) VALUES(@id_book,@date_pret,@date_retour,@id_client)", con);
+            cmd.Parameters.AddWithValue("@id_book", id_book);
+            cmd.Parameters.AddWithValue("@date_pret", date_pret.Value);
+            cmd.Parameters.AddWithValue("@date_retour", date_retour.Value);
+            cmd.Parameters.AddWithValue("@id_client", id_client);          
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Livre prêté avec succès");
             Close();
         }
     }
